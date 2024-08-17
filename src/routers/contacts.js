5d79ed73634c409d57b1498d97da23a2138ffb0f
zero <sanyafullgit@gmail.com>
2024-08-17 11:32:38 +0300

    fixed problems with routes

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -17,9 +17,9 @@ const router = Router();
 router.use(authenticate);
 
 router.get('/', ctrlWrapper(getAllContactsController));
-router.get('/contacts/:contactId', isValidId, ctrlWrapper(getContactByIdController));
-router.post('/contacts', validateBody(createContactSchema), ctrlWrapper(createContactController));
-router.patch('/contacts/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController)); 
-router.delete('/contacts/:contactId', isValidId, ctrlWrapper(deleteContactController)); 
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController)); 
+router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(patchContactController)); 
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContactController));
 
 export default router;
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
     name: Joi.string().min(3).max(20).required(),
-    phoneNumber: Joi.string().min(3).max(20).pattern(/^[0-9]+$/).min(10).max(15).required(),
+    phoneNumber: Joi.string().pattern(/^\+?[0-9]+$/).required(),
     email: Joi.string().email().required(),
     isFavourite: Joi.boolean().required(),
     contactType: Joi.string().valid('work', 'home', 'personal').required(),
